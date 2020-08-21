f550b522fcc11a87ea1eb371e9d3680912931e7c
DenysFedorovych <63046982+DenysFedorovych@users.noreply.github.com>
2020-08-21 18:57:03 +0300

    another commit

--- src/com/company/BaseballElimination.java
+++ src/com/company/BaseballElimination.java
@@ -2,38 +2,32 @@ package com.company;
 
 import edu.princeton.cs.algs4.BinarySearchST;
 import edu.princeton.cs.algs4.In;
-import java.util.ArrayList;
+
 import edu.princeton.cs.algs4.FlowNetwork;
 import edu.princeton.cs.algs4.FordFulkerson;
 import edu.princeton.cs.algs4.FlowEdge;
 
 public class BaseballElimination {
     private BinarySearchST<String,int[]> teams = new BinarySearchST<>();
+    private BinarySearchST<Integer,String> bckw = new BinarySearchST<>();
     private int[][] matches;
-    private FlowNetwork FN;
+    private int max=0;
     public BaseballElimination(String filename)
     {
         In in = new In(filename);
         String num = in.readLine();
         int b = Integer.parseInt(num);
         matches = new int[b][b];
-        FN = new FlowNetwork((b*(b-1))/2 + b + 2);
         while(in.hasNextLine()){
             String[] current = in.readLine().split(" ");
-            int[] a = new int[4];
-            a[0]=teams.size();
+            int[] a = new int[3];
             for(int i=1;i<4;i++){a[i-1]=Integer.parseInt(current[i]);}
             for(int i=4;i<4+matches.length;i++){
                 matches[teams.size()][i-4]=Integer.parseInt(current[i]);
             }
+            bckw.put(teams.size(),current[0]);
             teams.put(current[0],a);
         }
-        for(int i=0;i<b;i++)
-            for(int j=i;j<b;j++)
-            {
-                
-            }
-
     }
 // create a baseball division from given filename in format specified below
 
@@ -43,13 +37,13 @@ public class BaseballElimination {
     public Iterable<String> teams(){return teams.keys();}
 // all teams
 
-    public int wins(String team){return teams.get(team)[1];}
+    public int wins(String team){return teams.get(team)[0];}
 // number of wins for given team
 
-    public int losses(String team){return teams.get(team)[2];}
+    public int losses(String team){return teams.get(team)[1];}
 // number of losses for given team
 
-    public int remaining(String team){return teams.get(team)[3];}
+    public int remaining(String team){return teams.get(team)[2];}
 // number of remaining games for given team
 
     public int against(String team1, String team2){return matches[teams.get(team1)[0]][teams.get(team2)[0]];}
